87c2039681f0f27c82c74a66aca1fe510bdf23ee
JamesK-Projects <63878760+JamesK-Projects@users.noreply.github.com>
2020-09-22 11:46:58 -0600

    refactored app

--- src/LaptopCustomizeList/LaptopCustomizeCategory/LaptopItem/LaptopItem.js
+++ src/LaptopCustomizeList/LaptopCustomizeCategory/LaptopItem/LaptopItem.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-// Normalizes string as a slug - a string that is safe to use
-// in both URLs and html attributes
-import slugify from 'slugify';
-
 // This object will allow us to
 // easily convert numbers into US dollar values
 const USCurrencyFormat = new Intl.NumberFormat('en-US', {
@@ -14,17 +10,16 @@ const USCurrencyFormat = new Intl.NumberFormat('en-US', {
 function LaptopItem(props){
     return (
         <div className="feature__item" onClick={e => props.updateFeature(props.feature, props.item)}>
-              <input
-                type="radio"
-                className="feature__option"
-                name={props.item.name}
-                checked={props.item.name === props.selected[props.feature].name}
-                
-              />
-              <label className="feature__label">
-                {props.item.name} ({USCurrencyFormat.format(props.item.cost)})
-              </label>
-            </div>
+            <input
+            type="radio"
+            className="feature__option"
+            name={props.item.name}
+            checked={props.item.name === props.selected[props.feature].name}
+            />
+            <label className="feature__label">
+            {props.item.name} ({USCurrencyFormat.format(props.item.cost)})
+            </label>
+        </div>
     )
 }
  
