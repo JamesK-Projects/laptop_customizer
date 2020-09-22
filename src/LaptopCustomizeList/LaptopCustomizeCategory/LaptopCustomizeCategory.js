33add5b4c5aef17675f07fe5cc7c990f5f93dcb0
JamesK-Projects <63878760+JamesK-Projects@users.noreply.github.com>
2020-09-22 11:46:24 -0600

    refactored app

--- src/LaptopCustomizeList/LaptopCustomizeCategory/LaptopCustomizeCategory.js
+++ src/LaptopCustomizeList/LaptopCustomizeCategory/LaptopCustomizeCategory.js
@@ -3,31 +3,27 @@ import LaptopItem from './LaptopItem/LaptopItem';
 
 class LaptopCustomizeCategory extends Component {
     render() { 
-        //const features = Object.keys(this.props.features).map((feature, idx) => {
-            return (
-                <div>
-                    <fieldset className="feature" 
-                    //key={featureHash}
-                    >
-                        <legend className="feature__name">
-                            <h3>{this.props.feature}</h3>
-                        </legend>
-                    </fieldset>
-                    {
-                        this.props.items.map((item) => {
-                            return <LaptopItem 
+        return (
+            <div>
+                <fieldset className="feature">
+                    <legend className="feature__name">
+                        <h3>{this.props.feature}</h3>
+                    </legend>
+                </fieldset>
+                {
+                    this.props.items.map((item) => {
+                        return (
+                            <LaptopItem 
                                 selected={this.props.selected}
                                 updateFeature={this.props.updateFeature}
                                 item={item}
                                 feature={this.props.feature}
-                    />
-                        })
-                    }
-                    
-                </div>
-            );
-        //})
-        
+                            />
+                        )
+                    })
+                }
+            </div>
+        )
     }
 }
  
