9360bce7a79c8788826e9bc3b5d10490a7e42bb4
JamesK-Projects <63878760+JamesK-Projects@users.noreply.github.com>
2020-09-22 11:48:05 -0600

    refactored app

--- src/Cart/CartItem/CartItem.js
+++ src/Cart/CartItem/CartItem.js
@@ -1,36 +1,21 @@
 import React from 'react';
 
-function CartItem(){
+function CartItem(props){
     const USCurrencyFormat = new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD'
     });
-    const total = Object.keys(this.props.selected).reduce(
-        (acc, curr) => acc + this.state.selected[curr].cost,
-        0
-    );
-    for (let i = 0; i < this.props.selected.length; i++){
-        return (
-            <div>
-                <div className="summary__option" 
-                    //key={featureHash}
-                >
-                
-                    <div className="summary__option__label">{this.props.selected[i]} </div>
-                    <div className="summary__option__value">{this.props.selected[i].name}</div>
-                    <div className="summary__option__cost">
-                        {USCurrencyFormat.format(this.props.selected[i].cost)}
-                    </div>
-                </div>
-                <div className="summary__total">
-                    <div className="summary__total__label">Total</div>
-                    <div className="summary__total__value">
-                        {USCurrencyFormat.format(total)}
-                    </div>
-                </div>
+    return (
+        <div>
+            <div className="summary__option">
+                <div className="summary__option__label">{props.items[0]} </div>         
+                <div className="summary__option__value">{props.items[1].name}</div>
+                <div className="summary__option__cost">
+                    {USCurrencyFormat.format(props.items[1].cost)}
+                </div>    
             </div>
-        )
-    }
+        </div>
+    )    
 }
 
 export default CartItem;
