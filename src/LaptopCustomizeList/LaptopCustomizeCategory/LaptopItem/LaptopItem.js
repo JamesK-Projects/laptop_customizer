70b251c0b3fdd84b948ec44e792ca82935e689a1
JamesK-Projects <63878760+JamesK-Projects@users.noreply.github.com>
2020-10-02 18:34:40 -0400

    fixed errors

--- src/LaptopCustomizeList/LaptopCustomizeCategory/LaptopItem/LaptopItem.js
+++ src/LaptopCustomizeList/LaptopCustomizeCategory/LaptopItem/LaptopItem.js
@@ -11,10 +11,11 @@ function LaptopItem(props){
     return (
         <div className="feature__item" onClick={e => props.updateFeature(props.feature, props.item)}>
             <input
-            type="radio"
-            className="feature__option"
-            name={props.item.name}
-            checked={props.item.name === props.selected[props.feature].name}
+                onChange={() => {}}
+                type="radio"
+                className="feature__option"
+                name={props.item.name}
+                checked={props.item.name === props.selected[props.feature].name}
             />
             <label className="feature__label">
             {props.item.name} ({USCurrencyFormat.format(props.item.cost)})
